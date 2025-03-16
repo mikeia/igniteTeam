879035a6ddc7ea470d3a16c8b85259d85a60c324
Mikeias <mikeiasilva@hotmail.com>
2025-03-16 14:01:04 -0300

    criação do component de loading e aplicando a condição ternaria para font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
 import { Groups } from '@screens/Groups';
 import  theme  from './src/theme'
 
 export default function App() {
+  const [ fontsLoaded] =  useFonts ({ Roboto_400Regular, Roboto_700Bold });
+
+
+
   return (
     <ThemeProvider theme={theme}>
-     <Groups/> 
+     { fontsLoaded ? <Groups/> : <Loading/>}
     </ThemeProvider>
   );
 }
