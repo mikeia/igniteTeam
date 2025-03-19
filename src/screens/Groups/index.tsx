d6b2a2a1ade33fef9b87f4ba6bae9f35750c28a9
Mikeias <mikeiasilva@hotmail.com>
2025-03-18 23:17:41 -0300

    Criando components GroupCard, HightLight e Header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/Header';
 import { Container } from './style';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
     <Container>
-      <Header/>
+      <Header />
+      <Highlight title='Turmas' subtitle='Jogue com sua turma'/>
+      <GroupCard title='Galera do TI'/>
+      <GroupCard title='Galera do Finan' />
     </Container>
   );
 }
