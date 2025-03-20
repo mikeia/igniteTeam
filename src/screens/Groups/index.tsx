ad2d208a1ee8f2530c7c474058a0751e591a7fcf
Mikeias <mikeiasilva@hotmail.com>
2025-03-19 23:26:42 -0300

    criação to flatlist e component Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,36 @@
+import { FlatList, Text } from 'react-native';
 import { Header } from '@components/Header';
 import { Container } from './style';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
       <Highlight title='Turmas' subtitle='Jogue com sua turma'/>
-      <GroupCard title='Galera do TI'/>
-      <GroupCard title='Galera do Finan' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) =>(
+          <GroupCard 
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex:1}}
+         ListEmptyComponent={() =>
+          <ListEmpty
+            message="Que tal cadastrar a primeira turma?"
+          />
+         }
+      />
+      
     </Container>
   );
 }
