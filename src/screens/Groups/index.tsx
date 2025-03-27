4290407a49b6419cd1ec86fa34a23afb28e5d097
Mikeias <mikeiasilva@hotmail.com>
2025-03-26 22:59:28 -0300

    Criando o componente de button com type props

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
 
@@ -30,7 +31,10 @@ export function Groups() {
           />
          }
       />
-      
+      <Button 
+          title="Adicionar"
+          type='SECONDARY'
+      />
     </Container>
   );
 }
